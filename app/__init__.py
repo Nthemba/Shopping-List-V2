7fbfb26bcb4f345067e025323e3b31ef27738c4d
kosgei <kosgei@mail.com>
2017-09-01 16:47:56 +0300

    [FEATURE] User accounts OOP features complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
+"""
+Initializes application
+"""
 from flask import Flask
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
+# secret key for Session
+app.secret_key = '\xacd\xf3\xe3\xb7\x06>\x86\x1d\x0b\x9c/\xfc)\xbd\xc57\x14\x80\xbc\xc2\x1d\x97h'
 
 # Load the views
 from app import views
